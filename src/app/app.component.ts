f2aae4a6e127defef95cdf0a70e6849e58c20749
davidmoradi <david@wishpond.com>
2017-11-12 20:41:46 -0800

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  rows = [];
+  loadingIndicator: boolean = true;
+  reorderable: boolean = true;
+
+  // columns = [
+  //   { prop: 'Name' },
+  //   { name: 'Price' },
+  //   { name: 'Short' }
+  // ];
+  private frontResult = {};
+  private apiUrl = 'http://coincap.io/front'
+  constructor(private http: HttpClient) {
+    this.fetch((data) => {
+      this.rows = data;
+      setTimeout(() => { this.loadingIndicator = false; }, 1500);
+    });
+  }
+
+  fetch(cb) {
+    const req = new XMLHttpRequest();
+    // req.open('GET', this.apiUrl);
+    req.open('GET', 'assets/sample_data/front.json');
+
+    req.onload = () => {
+      cb(JSON.parse(req.response));
+    };
+
+    req.send();
+  }
+
+  // getFront() {
+  //   // return this.http.get(this.apiUrl)
+  //   //   .map((res: HttpResponse) => res.json())
+  //   //   .subscribe(data => {
+  //   //     console.log(data)
+  //   //   })
+  //   this.http.get(this.apiUrl)
+  //     .subscribe(data => {
+  //       // Read the result field from the JSON response.
+  //       this.frontResult = data
+  //     });
+  // }
+  //
+  ngOnInit() {
+    console.log('component initiliazed')
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { enableProdMode } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NgxDatatableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
